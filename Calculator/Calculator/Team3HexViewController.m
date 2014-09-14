ce37703273cfc4a29c1e1b4dd5d0eb7a500a29ba
Jimmy Garcia <cooljj_20@sbcglobal.net>
2014-09-13 20:47:58 -0500

    Added ability to move screen for textField
    If the the chosen textfield is under the keyboard it will bring it up
    to where you need

--- Calculator/Calculator/Team3HexViewController.m
+++ Calculator/Calculator/Team3HexViewController.m
@@ -65,6 +65,31 @@
     return YES;
     
 }
+/* BEGIN MOVE textField FOCUS */
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    [self animateTextField: textField up: YES];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    [self animateTextField: textField up: NO];
+}
+
+- (void) animateTextField: (UITextField*) textField up: (BOOL) up
+{
+    const int movementDistance = 0; // tweak as needed
+    const float movementDuration = 0.3f; // tweak as needed
+    
+    int movement = (up ? -movementDistance : movementDistance);
+    
+    [UIView beginAnimations: @"anim" context: nil];
+    [UIView setAnimationBeginsFromCurrentState: YES];
+    [UIView setAnimationDuration: movementDuration];
+    self.view.frame = CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
+}
+/* END MOVE textField FOCUS */
 
 -(void)textFieldDidChange :(UITextField *)textField{
     //converts and sends text to other two text fields
