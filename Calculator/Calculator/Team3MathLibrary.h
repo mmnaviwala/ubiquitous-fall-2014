90ea7b4120338f86968fc1ab7fabe7a7b3b9b741
Gabriel Ohlson <gabeohlson@gmail.com>
2014-09-20 11:30:58 -0500

    Added Conversions to math library

--- Calculator/Calculator/Team3MathLibrary.h
+++ Calculator/Calculator/Team3MathLibrary.h
@@ -29,7 +29,43 @@
 + (int)factorial:(int)value;
 
 
+#pragma mark - Length
++ (double)convertKilometerToMeter:(double)numberToConvert;
++ (double)convertKilometerToCentimeter:(double)numberToConvert;
++ (double)convertMeterToKilometer:(double)numberToConvert;
++ (double)convertMeterToCentimeter:(double)numberToConvert;
++ (double)convertCentimeterToKilometer:(double)numberToConvert;
++ (double)convertCentimeterToMeter:(double)numberToConvert;
 
+#pragma mark - Mass
++ (double)convertMetricTonToKilogram:(double)numberToConvert;
++ (double)convertMetricTonToGram:(double)numberToConvert;
++ (double)convertMetricTonToMilligram:(double)numberToConvert;
++ (double)convertKilogramToMetricTon:(double)numberToConvert;
++ (double)convertKilogramToGram:(double)numberToConvert;
++ (double)convertKilogramToMilligram:(double)numberToConvert;
++ (double)convertGramToMetricTon:(double)numberToConvert;
++ (double)convertGramToKiloGram:(double)numberToConvert;
++ (double)convertGramToMilligram:(double)numberToConvert;
++ (double)convertMilligramToMetricTon:(double)numberToConvert;
++ (double)convertMilligramToKilogram:(double)numberToConvert;
++ (double)convertMilligramToGram:(double)numberToConvert;
+
+#pragma mark - Time
++ (double)convertDaysToHours:(double)numberToConvert;
++ (double)convertDaysToMinutes:(double)numberToConvert;
++ (double)convertHoursToDays:(double)numberToConvert;
++ (double)convertHoursToMinutes:(double)numberToConvert;
++ (double)convertMinutesToDays:(double)numberToConvert;
++ (double)convertMinutesToHours:(double)numberToConvert;
+
+#pragma mark - Temperature
++ (double)convertCelsiusToFahrenheit:(double)numberToConvert;
++ (double)convertCelsiusToKelvin:(double)numberToConvert;
++ (double)convertFahrenheitToCelsius:(double)numberToConvert;
++ (double)convertFahrenheitToKelvin:(double)numberToConvert;
++ (double)convertKelvinToCelsius:(double)numberToConvert;
++ (double)convertKelvinToFahrenheit:(double)numberToConvert;
 
 
 @end
