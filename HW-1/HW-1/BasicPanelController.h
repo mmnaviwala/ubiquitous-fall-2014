22344fbd428e69637b67d836706275452cab5408
Xiaolu Zhang <xiaolu@Xiaolus-MacBook-Pro.local>
2014-09-23 12:26:33 -0500

    basic panel

--- HW-1/HW-1/BasicPanelController.h
+++ HW-1/HW-1/BasicPanelController.h
@@ -10,5 +10,19 @@
 #import "CustomUIViewController.h"
 
 @interface BasicPanelController : CustomUIViewController
+@property (nonatomic) BOOL typingNumber; //check if user is typing a number
+@property (nonatomic) float firstNumber;
+@property (nonatomic) float secondNumber;
+@property (nonatomic, copy) NSString *operation; // +,-,*,/ operation
+
+- (IBAction)backSpace:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *calculationDisplay;
+
+- (IBAction)numberPressed:(UIButton *)sender;
+- (IBAction)clearPressed:(UIButton *)sender;
+- (IBAction)calculationPressed:(id)sender;
+- (IBAction)equalPressed:(UIButton *)sender;
+
+
 
 @end
