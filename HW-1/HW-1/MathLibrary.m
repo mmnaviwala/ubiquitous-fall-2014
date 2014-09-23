5eb417a32f7546c9db1f775a3c5a24b9a049f37b
Gabriel Ohlson <gabeohlson@gmail.com>
2014-09-23 17:03:46 -0500

    All operations working

--- HW-1/HW-1/MathLibrary.m
+++ HW-1/HW-1/MathLibrary.m
@@ -58,6 +58,66 @@
         return value * [MathLibrary factorial:(value-1)];
 }
 
++ (BOOL)is2x2Invertible:(NSArray *)matrix {
+    
+    if ( ([[matrix objectAtIndex:0] intValue] * [[matrix objectAtIndex:3] intValue] - [[matrix objectAtIndex:1] intValue] * [[matrix objectAtIndex:2] intValue]) == 0) {
+        return NO;
+    }
+    else {
+        return YES;
+    }
+}
+
++ (NSArray *)add2x2Matrix:(NSArray *)operand1 withOperand2:(NSArray *)operand2 {
+    int a = [[operand1 objectAtIndex:0] intValue] + [[operand2 objectAtIndex:0] intValue];
+    int b = [[operand1 objectAtIndex:1] intValue] + [[operand2 objectAtIndex:1] intValue];
+    int c = [[operand1 objectAtIndex:2] intValue] + [[operand2 objectAtIndex:2] intValue];
+    int d = [[operand1 objectAtIndex:3] intValue] + [[operand2 objectAtIndex:3] intValue];
+    
+    return @[ [[NSNumber alloc] initWithInt:a],
+                          [[NSNumber alloc] initWithInt:b],
+                          [[NSNumber alloc] initWithInt:c],
+                          [[NSNumber alloc] initWithInt:d] ];
+    
+}
++ (NSArray *)subtract2x2Matrix:(NSArray *)operand1 withOperand2:(NSArray *)operand2 {
+    int a = [[operand1 objectAtIndex:0] intValue] - [[operand2 objectAtIndex:0] intValue];
+    int b = [[operand1 objectAtIndex:1] intValue] - [[operand2 objectAtIndex:1] intValue];
+    int c = [[operand1 objectAtIndex:2] intValue] - [[operand2 objectAtIndex:2] intValue];
+    int d = [[operand1 objectAtIndex:3] intValue] - [[operand2 objectAtIndex:3] intValue];
+    
+    return @[ [[NSNumber alloc] initWithInt:a],
+              [[NSNumber alloc] initWithInt:b],
+              [[NSNumber alloc] initWithInt:c],
+              [[NSNumber alloc] initWithInt:d] ];
+    
+}
++ (NSArray *)multiply2x2Matrix:(NSArray *)operand1 withOperand2:(NSArray *)operand2 {
+    int a = [[operand1 objectAtIndex:0] intValue] * [[operand2 objectAtIndex:0] intValue] + [[operand1 objectAtIndex:1] intValue] * [[operand2 objectAtIndex:2] intValue];
+    int b = [[operand1 objectAtIndex:0] intValue] * [[operand2 objectAtIndex:1] intValue] + [[operand1 objectAtIndex:1] intValue] * [[operand2 objectAtIndex:3] intValue];
+    int c = [[operand1 objectAtIndex:2] intValue] * [[operand2 objectAtIndex:0] intValue] + [[operand1 objectAtIndex:3] intValue] * [[operand2 objectAtIndex:2] intValue];
+    int d = [[operand1 objectAtIndex:2] intValue] * [[operand2 objectAtIndex:1] intValue] + [[operand1 objectAtIndex:3] intValue] * [[operand2 objectAtIndex:3] intValue];
+    return @[ [[NSNumber alloc] initWithInt:a],
+              [[NSNumber alloc] initWithInt:b],
+              [[NSNumber alloc] initWithInt:c],
+              [[NSNumber alloc] initWithInt:d] ];
+    
+}
++ (NSArray *)divide2x2Matrix:(NSArray *)operand1 withOperand2:(NSArray *)operand2 {
+    
+    int determinate = [[operand2 objectAtIndex:0] intValue] * [[operand2 objectAtIndex:3] intValue] - [[operand2 objectAtIndex:1] intValue] * [[operand2 objectAtIndex:2] intValue];
+    
+    
+    NSArray *inversedOperand2 = @[ [[NSNumber alloc] initWithInt: ([[operand2 objectAtIndex:0] intValue] / determinate )],
+                                   [[NSNumber alloc] initWithInt: ([[operand2 objectAtIndex:1] intValue] / determinate )],
+                                   [[NSNumber alloc] initWithInt: ([[operand2 objectAtIndex:2] intValue] / determinate )],
+                                   [[NSNumber alloc] initWithInt: ([[operand2 objectAtIndex:3] intValue] / determinate )] ];
+    
+    return [MathLibrary multiply2x2Matrix:operand1 withOperand2:inversedOperand2];
+    
+}
+
+
 #pragma mark - Length
 + (double)convertKilometerToMeter:(double)numberToConvert{
     return (numberToConvert*1000);
