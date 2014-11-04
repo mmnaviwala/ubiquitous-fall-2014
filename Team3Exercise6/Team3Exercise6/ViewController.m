f8415c6ffeb7f550c3ff06bd68125083ee44d700
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-11-03 21:47:52 -0600

    Tracking current image

--- Team3Exercise6/Team3Exercise6/ViewController.m
+++ Team3Exercise6/Team3Exercise6/ViewController.m
@@ -16,6 +16,7 @@
 @property (strong, nonatomic) UIImageView *currentImage;
 @property (weak, nonatomic) IBOutlet UILabel *indexController;
 @property (strong, nonatomic) NSArray *photos;
+@property (weak, nonatomic) IBOutlet UIImageView *imageToDisplay;
 @property BOOL newMedia;
 @end
 
@@ -49,6 +50,12 @@
 - (void) scrollViewDidScroll:(UIScrollView *)scrollView{
     int currentIndex = self.scrollView.contentOffset.x / scrollView.frame.size.width;
     self.indexController.text = [NSString stringWithFormat:@"%i of %i", currentIndex+1, [self.photos count]];
+    
+    
+    self.currentImage = [self.photos objectAtIndex:currentIndex];
+    NSLog(@"%@", NSStringFromClass([[self.photos objectAtIndex:currentIndex] class]));
+    self.imageToDisplay.image = self.currentImage.image;
+    [self filterSepia];
 }
 
 -(void) viewWillAppear:(BOOL)animated{
@@ -58,26 +65,43 @@
     
 }
 
+- (void)filterSepia {
+    CIImage *beginImage = [CIImage imageWithData: UIImagePNGRepresentation(self.currentImage.image)];
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone" keysAndValues: kCIInputImageKey, beginImage, nil];
+    CIImage *outputImage = [filter outputImage];
+    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
+        
+    self.currentImage.image = [UIImage imageWithCGImage:cgimg];
+    CGImageRelease(cgimg);
+}
+
 -(void)fetchPictures{
     NSMutableArray *collector = [[NSMutableArray alloc] initWithCapacity:0];
     ALAssetsLibrary *library = [self defaultAssetsLibrary];
     [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
         [group enumerateAssetsUsingBlock:^(ALAsset *asset, NSUInteger index, BOOL *stop) {
             if (asset) {
-                [collector addObject:asset];
+                // [collector addObject:asset];
                 // [self createNewImageView:asset];
                 ALAssetRepresentation *rep = [asset defaultRepresentation];
                 CGImageRef imageRef = [rep fullResolutionImage];
                 if (imageRef) {
-                    UIImage *someImage = [UIImage imageWithCGImage:imageRef scale:1.0 orientation:UIImageOrientationRight];
+                    UIImage *someImage = [UIImage imageWithCGImage:imageRef scale:1.0 orientation:UIImageOrientationUp];
                     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(self.scrollView.frame.size.width * index, 0, self.scrollView.frame.size.width, self.scrollView.frame.size.height)];
+                    imageView.contentMode = UIViewContentModeScaleAspectFit;
                     imageView.image = someImage;
+                    
+                    // =============================================
+                    // =============================================
+                    // =============================================
+                    [collector addObject:imageView];
                     
                     [self.scrollView addSubview:imageView];
                 }
                 
             }
-            NSLog(@"%i", index);
+//            NSLog(@"%i", index);
         }];
         self.photos = [collector copy];
         self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width * [self.photos count], self.scrollView.frame.size.height);
@@ -109,7 +133,8 @@
     if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {
         UIImage *image = info[UIImagePickerControllerOriginalImage];
         
-        self.currentImage.image = image;
+//        self.currentImage.image = image;
+        NSLog(@"HOLA!");
         if (self.newMedia)
             UIImageWriteToSavedPhotosAlbum(image,
                                            self,
