61fb18fd8b984941359438096a3a11aeab1f9384
Jimmy Garcia <cooljj_20@sbcglobal.net>
2014-11-09 14:32:42 -0600

    Added Classes

---
+++ SocialJournal/SocialJournal/EntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  EntryViewController.h
+//  SocialJournal
+//
+//  Created by James Garcia on 11/9/14.
+//  Copyright (c) 2014 UH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EntryViewController : UIViewController
+
+@end
