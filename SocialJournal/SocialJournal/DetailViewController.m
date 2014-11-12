88368cee35067de1da68f7de078a1c325b6e555a
Matt Phillips <mphil355@Matts-Mac.local>
2014-11-11 19:11:12 -0600

    remove #warning from tableview controllers attach hashtag detector to detailview/new entry view "submit" button

--- SocialJournal/SocialJournal/DetailViewController.m
+++ SocialJournal/SocialJournal/DetailViewController.m
@@ -49,12 +49,59 @@
     [self configureView];
 }
 
+/*
+ * Method: replaceWordInString(text:removed:replacement)
+ * pass text, the word to be removed, and the word to be replaced to this method
+ * it will return the text with the removed word replaced with the desired wored
+ */
+-(NSString*) replaceWordInString: (NSString*)text : (NSString*)removed : (NSString*)replacement{
+    return [text stringByReplacingOccurrencesOfString:removed withString:replacement];
+}
+
+/*
+ * Method: getTagsFromText(text)
+ * pass text to this method and it will return an array of hashtags
+ * a hashtag is any of the following:
+ * #yes #YESyesyes # no #n o #YesYes
+ * no whitespace in a hashtag
+ */
+- (NSMutableArray*) getTagsFromText: (NSString*)text{
+    //initialize the arrays
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    NSArray *wordArray = [text componentsSeparatedByString:@" "];
+    
+    //go through the text to find hashtags
+    for(int i = 0; i < wordArray.count; i++){
+        //if the first letter of a word starts with a hashtag
+        if([[wordArray[i] substringFromIndex:0] containsString:@"#"]){
+            NSString *temp = [wordArray objectAtIndex:i];
+            //more in depth hashtag checking to make sure its a valid format
+            //could potentially change to regex if someone wants to get fancy
+            if(temp.length > 1 &&
+               ![temp hasPrefix:@"##"] &&
+               ![[temp substringFromIndex:1] containsString:@"#"]){
+                
+                //add the correct hashtag to the result array
+                [result addObject:temp];
+                //NSLog(@"%@", result);
+            }
+        }
+    }
+    return result;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)submitButtonClicked:(id)sender {
+    
+    NSMutableArray *entryTags = [self getTagsFromText:self.journalEntryTextView.text];
+    NSMutableArray *titleTags = [self getTagsFromText:self.titleText.text];
+    NSArray *tags = [entryTags arrayByAddingObjectsFromArray:titleTags];
+    NSLog(@"%@", tags);
+    
     PFObject *eachEntry = [PFObject objectWithClassName:@"Entries"];
     eachEntry[@"title"] = self.titleText.text;
     eachEntry[@"entry"] = self.journalEntryTextView.text;
