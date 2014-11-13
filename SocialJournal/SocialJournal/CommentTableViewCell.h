113cce421622676f4ce365de5051ab413513d075
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-11-12 18:31:28 -0600

    Added tableview to display comments - Added xib file for comments cell

---
+++ SocialJournal/SocialJournal/CommentTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  CommentTableViewCell.h
+//  SocialJournal
+//
+//  Created by Muhammad Naviwala on 11/12/14.
+//  Copyright (c) 2014 UH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentTableViewCell : UITableViewCell
+//@property (weak, nonatomic) IBOutlet UILabel *username;
+//@property (weak, nonatomic) IBOutlet UILabel *postTitle;
+//@property (weak, nonatomic) IBOutlet UILabel *postPreview;
+//@property (weak, nonatomic) IBOutlet UILabel *postTags;
+//@property (weak, nonatomic) IBOutlet UILabel *postDate;
+//@property (weak, nonatomic) IBOutlet UIImageView *postFavorite;
+//@property (weak, nonatomic) IBOutlet UIImageView *profilePhoto;
+//@property (weak, nonatomic) IBOutlet UIView *cellContent;
+
+@end
+
---
+++ SocialJournal/SocialJournal/CommentTableViewCell.m
@@ -0,0 +1,27 @@
+//
+//  CommentTableViewCell.m
+//  SocialJournal
+//
+//  Created by Muhammad Naviwala on 11/12/14.
+//  Copyright (c) 2014 UH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CommentTableViewCell.h"
+
+@implementation CommentTableViewCell
+
+- (void)awakeFromNib {
+//    self.profilePhoto.layer.cornerRadius = 7.0f;
+//    self.profilePhoto.clipsToBounds = YES;
+//    [self.profilePhoto.layer setBorderColor: [[UIColor colorWithRed:(49/255.0) green:(50/255.0) blue:(51/255.0) alpha:1.0] CGColor]];
+//    [self.profilePhoto.layer setBorderWidth: 2.0];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+@end
