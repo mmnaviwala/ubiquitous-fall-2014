9cfd70a0f752272990123b31c758593c388bd67e
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-11-17 21:51:04 -0600

    Added FeedViewController and NotificationsViewController, and added those views in the storyboard

---
+++ SocialJournalSwift/SocialJournal/SocialJournal/FeedViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FeedViewController.swift
+//  SocialJournal
+//
+//  Created by Muhammad Naviwala on 11/17/14.
+//  Copyright (c) 2014 UH. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    var button: HamburgerButton! = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.button = HamburgerButton(frame: CGRectMake(20, 20, 60, 60))
+        self.button.addTarget(self, action: "toggle:", forControlEvents:.TouchUpInside)
+        self.button.addTarget(self.revealViewController(), action: "revealToggle:", forControlEvents: UIControlEvents.TouchUpInside)
+        // self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        var myCustomBackButtonItem:UIBarButtonItem = UIBarButtonItem(customView: self.button)
+        self.navigationItem.leftBarButtonItem  = myCustomBackButtonItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func toggle(sender: AnyObject!) {
+        self.button.showsMenu = !self.button.showsMenu
+    }
+    
+    
+}
+
