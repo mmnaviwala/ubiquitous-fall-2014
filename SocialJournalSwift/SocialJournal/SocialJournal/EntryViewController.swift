2c96a5837126e6aa0b60d4808a226da55722c92a
Matt Phillips <mphil355@Matts-Mac.local>
2014-11-20 09:34:21 -0600

    add market framework link location button in entry view start code for map

--- SocialJournalSwift/SocialJournal/SocialJournal/EntryViewController.swift
+++ SocialJournalSwift/SocialJournal/SocialJournal/EntryViewController.swift
@@ -36,6 +36,13 @@ class EntryViewController: UIViewController {
     }
     
 
+    @IBAction func locationButtonPressed(sender: AnyObject) {
+        
+        //let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        //let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        //self.map.setRegion(region, animated: true)
+    }
     /*
     // MARK: - Navigation
 
