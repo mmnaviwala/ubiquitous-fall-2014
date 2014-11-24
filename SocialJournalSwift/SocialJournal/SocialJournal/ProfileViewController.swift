da3ea1495ea05ed635a24643e17dc08a3c40b73c
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-11-23 19:04:01 -0600

    Added view entries segment in the profile view controller

--- SocialJournalSwift/SocialJournal/SocialJournal/ProfileViewController.swift
+++ SocialJournalSwift/SocialJournal/SocialJournal/ProfileViewController.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var currentUserProfilePicture: UIImageView!
     @IBOutlet weak var currentUserName: UILabel!
     @IBOutlet weak var theCollectionView: UICollectionView!
     @IBOutlet weak var noDataFoundLabel: UILabel!
     @IBOutlet weak var followButton: UIButton!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var theTableView: UITableView!
     
     var button: HamburgerButton! = nil
+    var allEntries = []
     
     // this needs to be set from somewhere else
     // so when other profiles, we can set the "currentUser"
@@ -56,6 +58,9 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         currentUserProfilePicture.layer.borderWidth = 6.0
         currentUserProfilePicture.layer.borderColor = UIColor.whiteColor().CGColor;
         
+        theTableView.hidden = true
+        theCollectionView.hidden = false
+        
         self.noDataFoundLabel.hidden = true
         if (currentUser.objectId == PFUser.currentUser().objectId){
             self.followButton.hidden = true
@@ -69,6 +74,16 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
             NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
                 self.spinner.stopAnimating()
                 self.theCollectionView.reloadData()
+            })
+        }
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            self.spinner.startAnimating()
+//            self.allEntries = ParseQueries.getAllEntriesForCurrentUser(PFUser.currentUser()) //We are no longer using this query, plase use queryForEntries
+            println(self.allEntries)
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.spinner.stopAnimating()
+                self.theTableView.reloadData()
             })
         }
         
@@ -88,8 +103,15 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         switch sender.selectedSegmentIndex {
         case 0:
             currentCollectionViewDataArray = followingArray
+            theTableView.hidden = true
+            theCollectionView.hidden = false
         case 1:
             currentCollectionViewDataArray = followersArray
+            theTableView.hidden = true
+            theCollectionView.hidden = false
+        case 2:
+            theTableView.hidden = false
+            theCollectionView.hidden = true
         default:
             break;
         }
@@ -159,4 +181,96 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
             sender.layer.backgroundColor = UIColor(red: 39.0/255, green: 154.0/255, blue: 216.0/255, alpha: 1.0).CGColor
         }
     }
+    
+    
+    
+    
+    
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.theTableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return self.allEntries.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:feedCellTableViewCell = tableView.dequeueReusableCellWithIdentifier("feedCell") as feedCellTableViewCell
+        cell.backgroundColor = UIColor.clearColor()
+        
+        if (self.allEntries != []){
+            var entry:PFObject = self.allEntries[indexPath.section] as PFObject
+            
+            //Date
+            var weekday: NSDateFormatter = NSDateFormatter()
+            var day: NSDateFormatter = NSDateFormatter()
+            var month: NSDateFormatter = NSDateFormatter()
+            var year: NSDateFormatter = NSDateFormatter()
+            weekday.setLocalizedDateFormatFromTemplate("EEEE")
+            day.setLocalizedDateFormatFromTemplate("dd")
+            month.setLocalizedDateFormatFromTemplate("MMMM")
+            year.setLocalizedDateFormatFromTemplate("YYYY")
+            var dateStringWeekday: NSString = weekday.stringFromDate(entry.createdAt)
+            var dateStringDay: NSString = day.stringFromDate(entry.createdAt)
+            var dateStringMonth: NSString = month.stringFromDate(entry.createdAt)
+            var dateStringYear: NSString = year.stringFromDate(entry.createdAt)
+            
+            cell.username.text = "anonDawg"
+            //        cell.userProfilePicture.image =
+            
+            //        if(favorited) {
+            //            cell.hearted.image =
+            //        }
+            var entryTitle:String = entry["title"] as String!
+            var entryText:String = entry["content"] as String!
+            
+            cell.heartCount.text = String(ParseQueries.getHeartCountForEntry(entry))
+            cell.postTitle.text = entryTitle
+            cell.postBody.text = entryText
+            cell.dateWeekday.text = dateStringWeekday
+            cell.dateDay.text = dateStringDay
+            cell.dateMonth.text = dateStringMonth
+            cell.dateYear.text = dateStringYear
+            
+        }
+        
+        return cell
+    }
+    
+    // UITableViewDelegate methods
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?{
+        var headerView = UIView(frame: CGRectMake(0, 0, tableView.bounds.size.width, 1))
+        headerView.backgroundColor = UIColor.clearColor()
+        
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat{
+        return 30.0
+    }
+    
+    func tableView(tableView:UITableView!, heightForRowAtIndexPath indexPath:NSIndexPath)->CGFloat
+    {
+        return 150
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+//        
+//        if segue.identifier == "feedToEntry"{
+//            var selectedRowIndexPath: NSIndexPath = self.theTableView.indexPathForSelectedRow()!
+//            var selectedSection: NSInteger = selectedRowIndexPath.section
+//            
+//            let vc = segue.destinationViewController as EntryViewController
+//            vc.entry = self.allEntries[selectedSection] as PFObject
+//        }
+//    }
 }
