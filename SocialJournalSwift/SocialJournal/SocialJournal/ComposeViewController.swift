f00ab79e7e98ecee913b05437eb19894fb7cea90
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-12-11 09:41:43 -0600

    Fixed a bug where hashtags were duplicated when saved to parse

--- SocialJournalSwift/SocialJournal/SocialJournal/ComposeViewController.swift
+++ SocialJournalSwift/SocialJournal/SocialJournal/ComposeViewController.swift
@@ -167,7 +167,9 @@ class ComposeViewController: UIViewController, CLLocationManagerDelegate, UINavi
         
         for match in contentMatches{
             var eachHashTag = (str as NSString).substringWithRange(match.range)
-            allTags.append(eachHashTag as String)
+            if !contains(allTags, eachHashTag){
+                allTags.append(eachHashTag as String)
+            }
         }
         
         // println(allTags)
